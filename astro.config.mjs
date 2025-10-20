308e71ad27308dcfb261516fc0c6f7d0f97c0b06
Sridhar Rao <13954344+raoshridhar90@users.noreply.github.com>
2025-10-19 22:14:06 -0400

    init: astro portfolio

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+﻿// @ts-check
+import { defineConfig } from 'astro/config';
+export default defineConfig({
+    site: 'http://localhost:4321',
+    base: '/', // local at root; we can change for GitHub Pages later
+});
+
+
+
+
